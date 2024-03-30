5c4a0f7b31a2b7f93fa4d4e1d32ed15c4aa6a36a
“MaguiMaluff” <maguimalu4@gmail.com>
2024-03-29 23:29:32 -0300

    Usuarios y Libros agrgados

---
+++ backend/main/resources/libros.py
@@ -0,0 +1,34 @@
+from flask_restful import Resource
+from flask import request
+
+LIBROS = {}
+
+class Libro(Resource):
+    def get(self, id):
+        if id in LIBROS:
+            return LIBROS(id)
+        return 'No existe el id', 404
+    
+    def delete(self, id):
+        if id in LIBROS:
+            del LIBROS[id]
+            return '', 204
+        return 'No existe el id', 404
+    
+    def put(self, id):
+        if id in LIBROS:
+            libro = LIBROS(id)
+            data = request.get_json()
+            libro.update(data)
+            return '', 201
+        return 'No existe el id', 404
+
+class Libros(Resource):
+    def get(self):
+        return LIBROS
+    
+    def post(self):
+        libro = request.get_json()
+        id = int(max(LIBROS.keys())) + 1
+        LIBROS[id] = libro
+        return LIBROS[id], 201
---
+++ backend/main/resources/usuario.py
@@ -0,0 +1,34 @@
+from flask_restful import Resource
+from flask import request
+
+USUARIOS = {}
+
+class Usuario(Resource):
+    def get(self, id):
+        if id in USUARIOS:
+            return USUARIOS(id)
+        return 'No existe el id', 404
+    
+    def delete(self, id):
+        if id in USUARIOS:
+            del USUARIOS[id]
+            return '', 204
+        return 'No existe el id', 404
+    
+    def put(self, id):
+        if id in USUARIOS:
+            usuario = USUARIOS(id)
+            data = request.get_json()
+            usuario.update(data)
+            return '', 201
+        return 'No existe el id', 404
+
+class Usuarios(Resource):
+    def get(self):
+        return USUARIOS
+    
+    def post(self):
+        usuario = request.get_json()
+        id = int(max(USUARIOS.keys())) + 1
+        USUARIOS[id] = usuario
+        return USUARIOS[id], 201
