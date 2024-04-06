3677a1014e89d11b571d89bd8cb946bf3a88cc3d
MaguiMaluff <maguimalu4@gmail.com>
2024-04-06 12:16:33 -0300

    Valoracion get funciona

--- backend/main/resources/valoraciones.py
+++ backend/main/resources/valoraciones.py
@@ -1,34 +1,42 @@
 from flask_restful import Resource
 from flask import request
 from .libros import LIBROS, Libro, Libros
+from .usuario import USUARIOS
 
 VALORACIONES = {
     1:{'valoracion': '***',
-       'usuario': '1',
+       'usuario': 1,
        'libro': '2'
        },
     2:{'valoracion': '**',
-       'usuario': '1',
+       'usuario': 1,
        'libro': '1'
        },
     3:{'valoracion': '***',
-       'usuario': '2',
+       'usuario': 2,
        'libro': '2'
        },
     4:{'valoracion': '**',
-       'usuario': '4',
+       'usuario': 4,
        'libro': '1'
        }
 }
 
 class Valoracion(Resource):
-    def get(id_user):
+    def get(self, id_user):
+        id_user = int(id_user)
         user_com = {}
+        lista = list(USUARIOS.keys())
+        if id_user not in lista:
+            return "El id no existe", 404
         for i in VALORACIONES:
             if id_user == VALORACIONES[i]['usuario']:
                 valoracion = VALORACIONES[i]
                 user_com[i] = valoracion
-        print(user_com)
+        if user_com == {}:
+            return "El usuario no tiene valoraciones"
+        else:
+            return "Valoracion/es del usuario: {}".format(user_com)
 
 class Valoraciones(Resource):
     def get(self):
