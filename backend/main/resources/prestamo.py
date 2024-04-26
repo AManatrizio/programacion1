7821cbaabc53e95e76ea506420f735569cf2cdb6
MaguiMaluff <maguimalu4@gmail.com>
2024-04-25 22:27:13 -0300

    Prestamo ya da errores en post

--- backend/main/resources/prestamo.py
+++ backend/main/resources/prestamo.py
@@ -3,6 +3,12 @@ from flask import request, jsonify
 from main.models import PrestamoModel
 from .. import db
 
+class IdEnUso(Exception):
+    ...
+
+class LibroNoDisponible(Exception):
+    ...
+
 class Prestamo(Resource):
     def get(self, id):
         prestamo = db.session.query(PrestamoModel).get_or_404(id)
@@ -36,11 +42,26 @@ class Prestamos(Resource):
         prestamos = []
         for prestamo_data in data:
             prestamo = PrestamoModel.from_json(prestamo_data)
-#            try:
-#                db.session.add(prestamo)
-#                prestamos.append(prestamo)
-#            except:
-#                return "Error"
+            try:
+                tabla = PrestamoModel.query.all()
+                self.verificacion(prestamo_data, tabla)
+            except Exception as e:
+                return {'error': str(e)}, 403
+            db.session.add(prestamo)
+            prestamos.append(prestamo)
         db.session.commit()
         prestamos_json = [prestamo.to_json() for prestamo in prestamos]
         return prestamos_json, 201
+
+    def verificacion(self, prestamo, tabla):
+        for i in tabla:
+            id = i.id
+            id_nuevo = prestamo['id']
+            id_libro = i.libro_id
+            id_libro_nuevo = prestamo['libro_id']
+            if id_libro == id_libro_nuevo:
+                raise LibroNoDisponible('El libro no esta disponible')
+            elif id == id_nuevo:
+                raise IdEnUso('El ID esta en uso')
+            else:
+                return None
