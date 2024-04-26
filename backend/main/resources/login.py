2aa04a1c71339ec6990e5b2433dae072de2dae9e
s.soler <s.soler@alumno.um.edu.ar>
2024-04-26 16:36:40 -0300

    Respuesta a errores login y signin

--- backend/main/resources/login.py
+++ backend/main/resources/login.py
@@ -1,9 +1,12 @@
-from flask_restful import Resource
+from flask_restful import Resource, abort
 from flask import request
 from main.models import UsuarioModel
 from .. import db
 
 class LogIn(Resource):
     def post(self, id):
-        usuario = db.session.query(UsuarioModel).get_or_404(id)
-        return 'Sesion iniciada', 201
+        try:
+            usuario = db.session.query(UsuarioModel).get_or_404(id)
+            return 'Sesion iniciada', 201
+        except Exception: #Si la URL tiene un id para iniciar sesion que no existe, entonces da error
+            abort(500, message=str("Error 404: el id del usuario no existe, cree una cuenta"))
--- backend/main/resources/signin.py
+++ backend/main/resources/signin.py
@@ -1,13 +1,30 @@
 from flask_restful import Resource
 from flask import request
 from main.models import UsuarioModel
+from main.resources import UsuariosResource
 from .usuario import Usuarios
 from .. import db
 
 class SignIn(Resource):
-    def post(self, id):
-        usuario = db.session.query(UsuarioModel).get(id)
-        if usuario:
-            return 'El usuario ya existe', 404
+    def post(self):
+        data = request.get_json()
+        usuario_id = data.get('id')
+
+        # Verificar si el usuario ya existe en la base de datos
+        usuario_existente = db.session.query(UsuarioModel).filter_by(id=usuario_id).first()
+        if usuario_existente:
+            return 'El id de usuario ya existe', 404
         else:
-            Usuarios.post()
+            # Si el usuario no existe, crea el usuario y lo agrega a la tabla de usuarios
+            nuevo_usuario = UsuarioModel.from_json(data)
+            db.session.add(nuevo_usuario)
+            db.session.commit()
+            return nuevo_usuario.to_json(), 201
+
+#Asi era con el id en la URL
+    # def post(self,id):
+    #     usuario = db.session.query(UsuarioModel).get(id)
+    #     if usuario: #SI el id del usuario ya existe:
+    #         return 'El id de usuario ya existe', 404
+    #     else: #Crea el usuario y lo agrega a la tabla usuarios
+    #         Usuarios.post(Usuarios())
