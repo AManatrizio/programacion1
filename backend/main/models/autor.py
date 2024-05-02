e46efb64d4ff66b04f9b737c159e2f6eb6d251a0
MaguiMaluff <maguimalu4@gmail.com>
2024-05-02 10:24:28 -0300

    Limpieza del codigo

--- backend/main/models/autor.py
+++ backend/main/models/autor.py
@@ -1,11 +1,10 @@
 from .. import db
 
 
-class Autores(db.Model): #e
+class Autores(db.Model): 
     id = db.Column(db.Integer, primary_key = True)
     autor = db.Column(db.String(100), nullable = False)
     
-
     def __repr__(self):
         return ('<Autor: %r >' % (self.autor) )
 
@@ -22,33 +21,3 @@ class Autores(db.Model): #e
         autor = autor_json.get('autor')
         return Autores(id = id,
                     autor = autor,)
-
-#---------------------------------------------------------------------------------------------
-# from .. import db
-
-# class Autores(db.Model):
-#     id = db.Column(db.Integer, primary_key = True)
-#     autor = db.Column(db.String(100), nullable = False)
-
-#     def __repr__(self):
-#         return ('<Autor: %r >' % (self.autor) )
-
-#     def to_json(self):
-#         autor_json = {
-#             'id': self.id,
-#             'autor': str(self.autor),
-#         }
-#         return autor_json
-    
-#     @staticmethod
-#     def from_json(autor_json):
-#         id = autor_json.get('id')
-#         autor = autor_json.get('autor')
-#         return Autores(id = id,
-#                     autor = autor,)
-
-# autor_libro = db.Table(
-#     'autor_libro',
-#     db.Column('autor', db.Integer, db.ForeignKey("autores.id"), nullable = False),
-#     db.Column('libro', db.Integer, db.ForeignKey("libros.id"), nullable = False)
-#     )
--- backend/main/models/libro.py
+++ backend/main/models/libro.py
@@ -12,8 +12,6 @@ class Libros(db.Model):
     genero = db.Column(db.String(100), nullable = False)
     
     autores = db.relationship('Autores', secondary=autor_libro , backref=db.backref('libros', lazy='dynamic'))
-
-
     prestamo = db.relationship("Prestamos", back_populates = "libro", cascade = "all, delete-orphan")
 
     def __repr__(self):
@@ -38,44 +36,3 @@ class Libros(db.Model):
             
   }
         return libro_json
-
-
-
-#-------------------------------------------------------------------------------------------------------------
-# from .. import db
-
-# class Libros(db.Model):
-#     id = db.Column(db.Integer, primary_key = True)
-#     nombre = db.Column(db.String(100), nullable = False)
-#     genero = db.Column(db.String(100), nullable = False)
-#     estado = db.Column(db.String(100), nullable = False)
-
-#     prestamo = db.relationship("Prestamos", back_populates = "libro", cascade = "all, delete-orphan")
-    
-
-
-#     def __repr__(self):
-#         return ('<Libro: %r >' % (self.nombre) )
-
-#     def to_json(self):
-#         libro_json = {
-#             'id': self.id,
-#             'nombre': str(self.nombre),
-#             'genero': str(self.genero),
-#             'estado': str(self.estado),
-#         }
-#         return libro_json
-    
-#     @staticmethod
-#     def from_json(libro_json):
-#         id = libro_json.get('id')
-#         nombre = libro_json.get('nombre')
-#         genero = libro_json.get('genero')
-#         estado = libro_json.get('estado')
-#         return Libros(id = id,
-#                     nombre = nombre,
-#                     genero = genero,
-#                     estado = estado,)
-
-# ###comentarios = db.relationship("Comentario", back_populates = "libro", cascade = "all, delete-orphan")
-# ###valoraciones = db.relationship("ValUsLib", back_populates = "libro", cascade = "all, delete-orphan")
