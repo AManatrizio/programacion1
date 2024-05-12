071cca6a83d574d79adf385af9b2074617892c4f
MaguiMaluff <maguimalu4@gmail.com>
2024-05-11 23:30:03 -0300

    Filtrado de opiniones arreglado. Cambio en prestamo en to_json_short

--- backend/main/resources/opinion.py
+++ backend/main/resources/opinion.py
@@ -41,46 +41,52 @@ class Opinion(Resource):
 class Opiniones(Resource):
     def get(self):
         page = 1
-        per_page = 10
+        per_page = 5
         opiniones = db.session.query(OpinionModel)
-        
+            
         if request.args.get('page'):
             page = int(request.args.get('page'))
         if request.args.get('per_page'):
             per_page = int(request.args.get('per_page'))
-
-        if request.args.get('id'):
-            opinion_valoracion = request.args.get('valoracion')
-            opiniones = opiniones.filter(OpinionModel.id == opinion_valoracion)
         
+        if request.args.get('valoracion'):
+            cant_ast = int(request.args.get('valoracion'))
+            valoracion_a_buscar = '*' * cant_ast
+            opiniones = opiniones.filter(OpinionModel.valoracion == valoracion_a_buscar)
         
         opiniones = opiniones.paginate(page=page, per_page=per_page, error_out=True)
-        
+
         return jsonify({'opiniones': [opinion.to_json() for opinion in opiniones],
                   'total': opiniones.total,
                   'pages': opiniones.pages,
                   'page': page
                 })
-    
-    def get(self):
-        try: 
-            opiniones = db.session.query(OpinionModel).all()
-            opiniones_json = [(opinion.to_json()) for opinion in opiniones]
-            return jsonify(opiniones_json)
-        except Exception:
-            abort(404, message=str("404 Not Found: No se encuentran opiniones"))
-
-
+        
 
     def post(self):
         data = request.get_json()
-        prestamo_id = data.get('prestamo_id')
-
-        prestamo_existente = db.session.query(PrestamoModel).filter_by(id=prestamo_id).first()
-        if not prestamo_existente:
-            return {'error': 'No se puede crear la opinión, el prestamo con ese id no existe'}, 404
-
-        opinion = OpinionModel.from_json(data)
-        db.session.add(opinion)
+        if isinstance(data, dict):
+            data = [data]
+        opinion_list = []
+        for opinion_data in data:
+            opinion = OpinionModel.from_json(opinion_data)
+            try:
+                tabla = OpinionModel.query.all()
+                self.verificacion(opinion_data, tabla)
+            except Exception as e:
+                return {'error': str(e)}, 403
+            db.session.add(opinion)
+            opinion_list.append(opinion)
         db.session.commit()
-        return opinion.to_json(), 201
+        opinion_json = [opinion.to_json() for opinion in opinion_list]
+        return opinion_json, 201
+
+
+    def verificacion(self, opinion, tabla):
+        for i in tabla:
+            id = i.id
+            id_nuevo = opinion['id']
+            if id == id_nuevo:
+                raise IdEnUso('El ID esta en uso')
+            else:
+                return None
