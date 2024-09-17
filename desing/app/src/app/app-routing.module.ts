32a5143852c88ec7031fce85083831534bdaedb4
s.soler <s.soler@alumno.um.edu.ar>
2024-09-17 16:47:29 -0300

    Added Angular pages

--- desing/app/src/app/app-routing.module.ts
+++ desing/app/src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './page/home/home.component';
+import { LoansComponent } from './page/loans/loans.component';
+import { ErrorPageComponent } from './page/error-page/error-page.component';
+import { LoginComponent } from './page/login/login.component';
+import { SinginComponent } from './page/singin/singin.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home' , component: HomeComponent},
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+
+  { path: 'loans' , component: LoansComponent},
+
+  { path: 'login', component: LoginComponent},
+
+  { path: 'singin', component: SinginComponent},
+
+  { path: '**', redirectTo: 'error_page'},
+  { path: 'error_page', component: ErrorPageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- desing/app/src/app/app.module.ts
+++ desing/app/src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './page/home/home.component';
+import { LoansComponent } from './page/loans/loans.component';
+import { ErrorPageComponent } from './page/error-page/error-page.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { LoginComponent } from './page/login/login.component';
+import { SinginComponent } from './page/singin/singin.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoansComponent,
+    ErrorPageComponent,
+    FooterComponent,
+    NavbarComponent,
+    LoginComponent,
+    SinginComponent
   ],
   imports: [
     BrowserModule,
