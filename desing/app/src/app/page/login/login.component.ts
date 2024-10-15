111024195e87444f9f2bf9563ae3643930c2be32
s.soler <s.soler@alumno.um.edu.ar>
2024-10-15 10:55:35 -0300

    fixed navbar and commented login.component.ts

--- desing/app/src/app/page/login/login.component.ts
+++ desing/app/src/app/page/login/login.component.ts
@@ -26,41 +26,41 @@ export class LoginComponent {
 
   // EL ARCHIVO .TS IMPORTA FUNCIONALIDADES DE LOS SERVICIOS Y LOS .HTML IMPORTAN FUNCIONALIDADES DE LOS .TS
 
-  // El archivo .ts se va a definir
-  irAlLogin(dataLogin: any) {
-    // Suscribe: 3 estados (Next, Error, )
-    this.authService.login(dataLogin).subscribe({
-      next: (rta: any) => {
-        alert('Credenciales correctas!!!');
-        console.log('Exito: ', rta);
-        // Agrega el token al Local Storage
-        localStorage.setItem('token', rta.access_token);
+  // // El archivo .ts se va a definir
+  // irAlLogin(dataLogin: any) {
+  //   // Suscribe: 3 estados (Next, Error, )
+  //   this.authService.login(dataLogin).subscribe({
+  //     next: (rta: any) => {
+  //       alert('Credenciales correctas!!!');
+  //       console.log('Exito: ', rta);
+  //       // Agrega el token al Local Storage
+  //       localStorage.setItem('token', rta.access_token);
 
-        // Decodifica el token para obtener el valor de la clave "rol"
-        const decodedToken: any = jwt_decode(rta.access_token);
-        const rol = decodedToken.rol;
+  //       // Decodifica el token para obtener el valor de la clave "rol"
+  //       const decodedToken: any = jwt_decode(rta.access_token);
+  //       const rol = decodedToken.rol;
 
-        // Guarda el valor de la clave "rol" en el Local Storage
-        localStorage.setItem('rol', rol);
-        this.router.navigateByUrl('home');
-      },
-      error: (err: any) => {
-        alert('Usuario o contraseña incorrecta.');
-        console.log('Exito: ', err);
-        localStorage.removeItem('token');
-      },
-      complete: () => {
-        console.log('Fianlizo');
-      },
-    });
-  }
+  //       // Guarda el valor de la clave "rol" en el Local Storage
+  //       localStorage.setItem('rol', rol);
+  //       this.router.navigateByUrl('home');
+  //     },
+  //     error: (err: any) => {
+  //       alert('Usuario o contraseña incorrecta.');
+  //       console.log('Exito: ', err);
+  //       localStorage.removeItem('token');
+  //     },
+  //     complete: () => {
+  //       console.log('Fianlizo');
+  //     },
+  //   });
+  // }
 
-  submit() {
-    if (this.loginForm.valid) {
-      console.log('Datos del formulario: ', this.loginForm.value);
-      this.irAlLogin(this.loginForm.value);
-    } else {
-      alert('Los valores son requeridos');
-    }
-  }
+  // submit() {
+  //   if (this.loginForm.valid) {
+  //     console.log('Datos del formulario: ', this.loginForm.value);
+  //     this.irAlLogin(this.loginForm.value);
+  //   } else {
+  //     alert('Los valores son requeridos');
+  //   }
+  // }
 }
